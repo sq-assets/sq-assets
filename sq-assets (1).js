4dc5435865202d55b1d965457fb80a34d045b333
sq-assets <bea@killvanillastudio.com>
2026-05-14 21:25:03 +0100

    Add files via upload

---
+++ sq-assets (1).js
@@ -0,0 +1,6 @@
+(function() {
+  var credit = document.createElement('div');
+  credit.style.cssText = 'text-align:center;font-size:12px;opacity:0.7;padding:20px 0;';
+  credit.innerHTML = 'WEBSITE DESIGNED BY <a href="https://www.killvanillastudio.com" target="_blank" style="text-decoration:none;color:inherit;">KILLVANILLASTUDIO.COM</a>';
+  document.body.appendChild(credit);
+})();
